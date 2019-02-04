f9c1360c652dede495b530791daf6bcbe47d6e99
Devin Sweeting <devinsweeting@gmail.com>
2019-02-04 14:10:44 -0800

    set up Songkick service to linking API calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { LocationsListComponent } from './locations-list/locations-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    LocationsListComponent
   ],
   imports: [
     BrowserModule,
