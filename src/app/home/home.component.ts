d341072c673912a0e7ff3d015cfef4d36224fe6d
Devin Sweeting <devinsweeting@gmail.com>
2019-02-05 13:02:31 -0800

    Add observable to spity API call

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { SongkickService } from '../services/songkick.service';
 import { Event } from '../models/event-model';
 import { SpotifyService } from '../services/spotify.service'
+import { Artist } from '../models/artist-model';
+import { Observable } from 'rxjs/Observable';
 
 
 
@@ -16,9 +18,8 @@ export class HomeComponent {
 locations: Location[] = null;
 eventDetails: any[] = null;
 eventsList: Event[] = [];
-
-
-albums: any[];
+artists: Observable<any>;
+artistIDs: string[];
 
 
   constructor(
@@ -26,32 +27,50 @@ albums: any[];
     public spotifyAPI: SpotifyService
   ) {}
 
-  getAccessToken() {
-    this.spotifyAPI.getToken()
-      .subscribe(res => {
-        this.spotifyAPI.searchAlbums("lil", res.access_token)
-          // tslint:disable-next-line:no-shadowed-variable
-          .subscribe(res => {
-            this.albums = res.albums.items;
-            console.log(this.albums);
-          });
+  getArtistsFromSpotify() {
+    return this.spotifyAPI.getToken().map(res => {
+        return this.spotifyAPI.searchArtistID("lil", res.access_token)
       });
+    // this.spotifyAPI.getToken()
+    //   .subscribe(res => {
+    //     this.spotifyAPI.searchArtistID("lil", res.access_token)
+    //       .subscribe(res => {
+    //         this.artists = res.artists.items;
+    //         // console.log(this.artists);
+    //         return this.artists = res.artists.items;
+    //       });
+    //   });
   }
-    findByDate(location: string, min: number, max: number) {
-      this.SongkickService.getLocationId(location).subscribe(response=>{
-        this.locations = response.json();
-        const id = this.locations.resultsPage.results.location[0].metroArea.id
-        this.SongkickService.filterByDate(id, min, max).subscribe(response=> {
-          this.eventDetails = response.json()
-          const events = this.eventDetails.resultsPage.results.event
-          events.forEach(event => {
-            const eventName = event.displayName;
-            const bandName = event.performance[0].artist.displayName
-            const newEvent = new Event(eventName, bandName)
-            this.eventsList.push(newEvent);
-          })
-          console.log(this.eventsList)
-        })
-      });
-    }
+
+  getIdsFromArtists(){
+    this.getArtistsFromSpotify().subscribe((artistListEmitted) => {
+      // artistListEmitted.subscribe((a)=> console.log(a.artists))
+console.log(this.artists)
+this.artists=artistListEmitted;
+      // for (let artist of artistListEmitted) {
+      //   this.artistIDs.push(artist.id)
+      //   console.log(artist.id);
+      // }
+
+    })
+    // console.log(this.artistIDs);
+  }
+
+  findByDate(location: string, min: number, max: number) {
+    // this.SongkickService.getLocationId(location).subscribe(response=>{
+    //   this.locations = response.json();
+    //   const id = this.locations.resultsPage.results.location[0].metroArea.id
+    //   this.SongkickService.filterByDate(id, min, max).subscribe(response=> {
+    //     this.eventDetails = response.json()
+    //     const events = this.eventDetails.resultsPage.results.event
+    //     events.forEach(event => {
+    //       const eventName = event.displayName;
+    //       const bandName = event.performance[0].artist.displayName
+    //       const newEvent = new Event(eventName, bandName)
+    //       this.eventsList.push(newEvent);
+    //     })
+    //     console.log(this.eventsList)
+    //   })
+    // });
+  }
 }
