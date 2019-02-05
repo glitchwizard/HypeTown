c14afb44074068c310082530f805420af9e6c48b
Student <example@example.com>
2019-02-05 13:24:48 -0800

    Move logic from homecomponent to songkick servic to handle api calls

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,43 +13,23 @@ export class HomeComponent {
 eventsAndBands: any[]=[];
 
 
-  constructor(private  SongkickService: SongkickService) {}
+  constructor(private  songkickService: SongkickService) {}
 
 
 
-  executeOnPerformance(shows) {
-    console.log("got into executeOnPerformance");
+  executeOnShows(shows) {
+    console.log("got into executeOnShows");
     shows.forEach(function(show) {
       console.log("show date: " + show.start.date);
-        show.performance.forEach(function(performance) {
-          console.log(" - " + performance.displayName)
-        })
+      show.performance.forEach(function(performance) {
+        console.log(" - " + performance.displayName)
+      })
     })
   }
 
-  findByDate(location: string, min: string, max: string) {
-    this.SongkickService.getLocationId(location).subscribe(response=>{
-      this.performancesByLocation(response,min,max)
-    });
-  }
-
-  performancesByLocation(response: any, min: string, max: string) {
-    const id = response.json().resultsPage.results.location[0].metroArea.id
-    this.SongkickService.filterByDate(id, min, max).subscribe(response=> {
-      this.reFilter(response.json(), min, max);
-    })
-  }
-
-  reFilter(response: any, min: string, max: string){
-    response.resultsPage.results.event.forEach((show)=> {
-      if((min<=(show.start.date))&&((show.start.date)<=max)) {
-        this.eventsAndBands.push(show);
-      }
-    });
-    this.executeOnPerformance(this.eventsAndBands)
-  }
-
-
+createPerformanceArray(location: string, min: string, max: string) {
+  this.songkickService.findByDate(location, min, max, this.executeOnShows)
+}
 
 
 }
