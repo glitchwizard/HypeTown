41583f0989bd73218c162ba81be07ae85f4fb0db
Student <example@example.com>
2019-02-06 11:12:24 -0800

    revert the code a bit to display JSON for deployment testing
    Co-Authored-By: Charley <glitchwizard@gmail.com>
    Co-Authored-By: Michael <jmichaelbrown132737@gmail.com>

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,17 +16,18 @@ import { HttpClient } from '@angular/common/http';
 
 export class HomeComponent {
   public showLocationQuery: string;
-  public showLocationIdResponse: Response;
   public showLocationId: string;
   public showLocation$: Observable<any>;
+  public showlocations$: Observable<any[]>;
+  public showMaxDate: string;
+  public showMinDate: string;
+
 
   constructor(private  songkickService: SongkickService, public spotifyService: SpotifyService, public client: HttpClient) {}
 
   findCityIdFromSongkick() {
-    console.log('something happened');
-
+    console.log('findCityIdFromSongkick() running...');
     this.showLocation$ = this.songkickService.getLocationIdFromAPI(this.showLocationQuery);
-
     return this.songkickService.getLocationIdFromAPI(this.showLocationQuery).map((response) => {
       this.showLocationId = response.resultsPage.results.location[0].metroArea.id;
       return response.resultsPage.results.location[0].metroArea.id
@@ -36,22 +37,26 @@ export class HomeComponent {
 //This function should change to use map instead of subscribe, we use subecribe for troubleshooting to log the outputs
 
   findListOfShowsByCityIdAndDateRange(location:string, minDate: string, maxDate: string) {
-    console.log('something happened 2');
-
+    console.log('findListOfShowsByCityIdAndDateRange() running...');
     this.showLocationQuery = location;
-
-    this.findCityIdFromSongkick().subscribe((idResponse) => {
-      console.log(idResponse);
-
-      this.songkickService.getShowListByCityIdAndDateRangeFromAPI(idResponse, minDate, maxDate).subscribe((showListResponse) => {
-        console.log('-----show list response-----')
-        for (let i = 0; i < 5; i++) {
-          console.log('---...---');
-          console.log(showListResponse.json().resultsPage.results.event[i]);
-        }
-        return showListResponse.json().resultsPage.results.event
-
+    return this.findCityIdFromSongkick()
+    .subscribe((idResponse) => {
+      return this.songkickService.getShowListByCityIdAndDateRangeFromAPI(idResponse, this.showMinDate, this.showMaxDate)
+      .subscribe((showListResponse) => {
+        console.log(showListResponse);
+        this.showlocations$ = showListResponse.resultsPage.results.event;
+        return showListResponse.resultsPage.results.event
       });
     })
+  }
+
+  generateArrayOfHeadlinerPerformances(location:string, minDate: string, maxDate: string){
+    console.log('generateArrayOfHeadlinerPerformances() running...');
+    this.showLocationQuery = location;
+    this.showMinDate = minDate;
+    this.showMaxDate = maxDate;
+    this.findListOfShowsByCityIdAndDateRange().get((showListResponse) => {
+      console.log(showListResponse);
+    });
   }
 }
