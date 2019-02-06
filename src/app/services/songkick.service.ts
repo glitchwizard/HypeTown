efc1f0e037e06a09b324166c510ee471cc9d2e1b
Student <example@example.com>
2019-02-06 10:17:20 -0800

    Working on using .map function to keep observables pushing data through
    Co-Authored-By: Charley <glitchwizard@gmail.com>
    Co-Authored-By: Michael <jmichaelbrown132737@gmail.com>

--- src/app/services/songkick.service.ts
+++ src/app/services/songkick.service.ts
@@ -10,38 +10,45 @@ const httpOptions = {
 
 @Injectable()
 export class SongkickService {
-eventsAndBands: any[]=[];
+  eventsAndBands: any[]=[];
 
   constructor(private http: Http) { }
 
-  getLocationId(query: string) {
-    return this.http.get(`https://api.songkick.com/api/3.0/search/locations.json?apikey=${songkickKey}&query=${query}`)
+  getLocationIdFromAPI(query: string) {
+    return this.http.get(`https://api.songkick.com/api/3.0/search/locations.json?apikey=${songkickKey}&query=${query}`).map((response: Response) => response.json())
   }
 
-  filterByDate(id, min, max){
+  getShowListByCityIdAndDateRangeFromAPI(id, min, max){
     return this.http.get(`https://api.songkick.com/api/3.0/metro_areas/${id}/calendar.json?apikey=${songkickKey}&$min_date=${min}&$max_date=${max}`)
   }
 
-
-  findShowsByDate(location: string, minDate: string, maxDate: string, component) {
-    this.getLocationId(location).subscribe(response=>{
-      this.performancesByLocation(response,minDate,maxDate, component)
-    });
-  }
-
-  performancesByLocation(response: any, min: string, max: string, component) {
-    const id = response.json().resultsPage.results.location[0].metroArea.id
-    this.filterByDate(id, min, max).subscribe(response=> {
-      this.filterPerformancesOutByShowDates(response.json(), min, max, component);
-    })
-  }
-
-  filterPerformancesOutByShowDates(response: any, min: string, max: string, component){
-    response.resultsPage.results.event.forEach((show)=> {
-      if((min<=(show.start.date))&&((show.start.date)<=max)) {
-        this.eventsAndBands.push(show);
-      }
-    });
-    component.executeOnShows(this.eventsAndBands)
-  }
+  // findShowsByDate(location: string, minDate: string, maxDate: string) {
+  //   return this.getLocationId(location);
+  // };
 }
+
+
+
+  // OLD Method
+  // findShowsByDate(location: string, minDate: string, maxDate: string, component) {
+  //   this.getLocationId(location).map( (response: Response) => {
+  //     this.performancesByLocation( response , minDate , maxDate , component)
+  //   });
+  // }
+
+  // performancesByLocation( response: Response, min: string, max: string, component) {
+  //   const id = response.json().resultsPage.results.location[0].metroArea.id
+  //   this.filterByDate(id, min, max).subscribe( (response: Response) => {
+  //     this.filterPerformancesOutByShowDates( response.json(), min, max, component);
+  //   })
+  // }
+  //
+  // filterPerformancesOutByShowDates(response: any, min: string, max: string, component){
+  //   response.resultsPage.results.event.forEach( (show)=> {
+  //     if((min<=(show.start.date))&&((show.start.date)<=max)) {
+  //       this.eventsAndBands.push(show);
+  //     }
+  //   });
+  //   component.executeOnShows(this.eventsAndBands)
+  // }
+// }
