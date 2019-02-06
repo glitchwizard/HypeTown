4b52365ed625819ff7f7561712cc46d7bbe5dfd8
Student <example@example.com>
2019-02-06 10:45:09 -0800

    cleaned up uncessary code/comments
    Co-Authored-By: Charley <glitchwizard@gmail.com>
    Co-Authored-By: Michael <jmichaelbrown132737@gmail.com>

--- src/app/services/songkick.service.ts
+++ src/app/services/songkick.service.ts
@@ -21,34 +21,4 @@ export class SongkickService {
   getShowListByCityIdAndDateRangeFromAPI(id, min, max){
     return this.http.get(`https://api.songkick.com/api/3.0/metro_areas/${id}/calendar.json?apikey=${songkickKey}&$min_date=${min}&$max_date=${max}`)
   }
-
-  // findShowsByDate(location: string, minDate: string, maxDate: string) {
-  //   return this.getLocationId(location);
-  // };
 }
-
-
-
-  // OLD Method
-  // findShowsByDate(location: string, minDate: string, maxDate: string, component) {
-  //   this.getLocationId(location).map( (response: Response) => {
-  //     this.performancesByLocation( response , minDate , maxDate , component)
-  //   });
-  // }
-
-  // performancesByLocation( response: Response, min: string, max: string, component) {
-  //   const id = response.json().resultsPage.results.location[0].metroArea.id
-  //   this.filterByDate(id, min, max).subscribe( (response: Response) => {
-  //     this.filterPerformancesOutByShowDates( response.json(), min, max, component);
-  //   })
-  // }
-  //
-  // filterPerformancesOutByShowDates(response: any, min: string, max: string, component){
-  //   response.resultsPage.results.event.forEach( (show)=> {
-  //     if((min<=(show.start.date))&&((show.start.date)<=max)) {
-  //       this.eventsAndBands.push(show);
-  //     }
-  //   });
-  //   component.executeOnShows(this.eventsAndBands)
-  // }
-// }
