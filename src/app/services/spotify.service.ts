c4e133d97a736ca54bef6318ff9d5495ce0eafe1
glitchwizard <40527412+glitchwizard@users.noreply.github.com>
2019-02-07 09:59:49 -0800

    Update home.component.ts
    Removed //TODO note, and moved getAllSpotifyArtistObjects() note to a more appropriate location.

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response, URLSearchParams } from "@angular/http";
+import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
+import {spotifyAPIclientSecret,spotifyAPIclientID} from '../api-keys'
+
+@Injectable()
+export class SpotifyService {
+  private searchUrl: string;
+  private encoded = btoa(spotifyAPIclientID + ':' + spotifyAPIclientSecret );
+
+  constructor(private _http: Http) { }
+
+  getToken() {
+    var params = ('grant_type=client_credentials');
+    var headers = new Headers();
+    headers.append('Authorization', 'Basic ' + this.encoded);
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    return this._http.post('https://accounts.spotify.com/api/token', params, { headers: headers })
+      .map(response => response.json());
+  }
+
+  searchArtistID(artistName: string, token: string) {
+      this.searchUrl = 'https://api.spotify.com/v1/search?query=' + artistName + '&type=artist';
+      let headers = new Headers();
+      headers.append('Authorization', 'Bearer ' + token);
+      return this._http.get(this.searchUrl, { headers: headers })
+        .map((token: Response) => {return token.json()})
+    }
+
+ // TODO: This function below needs to be finished.
+  findArtistTopTrack(id: string, token: string) {
+      this.searchUrl = 'https://api.spotify.com/v1/search?query=' + id + '&type=artist';
+      let headers = new Headers();
+      headers.append('Authorization', 'Bearer ' + token);
+      return this._http.get(this.searchUrl, { headers: headers })
+        .map((token: Response) => {return token.json()})
+    }
+
+
+}
